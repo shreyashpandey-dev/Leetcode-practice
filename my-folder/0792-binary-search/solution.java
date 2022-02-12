34997a1e391f4b5fa3fa232b2ca75877c4d8bad0
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2022-02-12 07:16:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (100.00%)

---
+++ my-folder/0792-binary-search/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) 
+    {
+        int low=0, high=nums.length-1, mid;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                //flag=1;
+                return mid;
+            }
+            else if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return -1;
+        
+        
+        
+    }
+}
