70d79e2b2510457d3d16afdde0f036e2bc9f5ecb
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2022-10-04 07:40:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.1 MB (100.00%)

---
+++ my-folder/2323-minimum-bit-flips-to-convert-number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+         start^=goal;
+     int count = 0;
+        while (start > 0) {
+            start &= (start - 1);
+            count++;
+        }
+        return count;
+    }
+}
