5710d5832dc12233bec10e867e74775a0e101556
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2022-10-07 10:11:23 +0000

    [LeetCode Sync] Runtime - 2 ms (1.47%), Memory - 41.1 MB (99.96%)

---
+++ my-folder/0231-power-of-two/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<0)
+            return false;
+        int count =0;
+       while(n!=0)
+       {
+          n&=n-1;
+           count++;
+       }
+        if(count==1)
+            return true;
+        return false;
+    }
+}
