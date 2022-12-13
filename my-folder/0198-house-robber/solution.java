c6ba0e4f69d80369215b5e3701da0baecca90a3e
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2022-12-13 12:46:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.88%)

---
+++ my-folder/0198-house-robber/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public int rob(int[] nums) {
+        int prev2=0,prev=nums[0],curr;
+       for(int i=1;i<nums.length;i++)
+       {
+           if(i==1)
+           {
+               curr=Math.max(nums[0],nums[1]);
+               prev2=nums[0];
+               prev=curr;
+               continue;
+           }
+       int pick=nums[i]+prev2;
+       int ntpick=0+prev;
+         curr=(int)Math.max(pick,ntpick);
+           prev2=prev;
+           prev=curr;
+    }
+        return prev;
+}
+}
