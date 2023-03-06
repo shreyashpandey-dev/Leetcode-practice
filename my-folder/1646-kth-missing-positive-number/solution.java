ef52118674c6832d72386aa2981b3bdfabdaaa80
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-03-06 17:35:30 +0000

    [LeetCode Sync] Runtime - 1 ms (25.89%), Memory - 42.6 MB (99.97%)

--- my-folder/1646-kth-missing-positive-number/solution.java
+++ my-folder/1646-kth-missing-positive-number/solution.java
@@ -1,12 +1,18 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        for(int i:arr)
+      int org[]=new int[10000];
+        for(int i=0;i<arr.length;i++)
+            org[arr[i]]=-1;
+        int y=0;
+        int i=1;
+        while(true)
         {
-            if(i<=k)
-                k++;
-            else
+            if(org[i]==0)
+                y++;
+            if(y==k)
                 break;
+            i++;
         }
-        return k;
+        return i;
     }
 }
