a3cd431c8168e73181860c0c63d8de8adbd6111b
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-03-09 06:18:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (100.00%)

---
+++ my-folder/0142-linked-list-cycle-ii/solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null)
+            return null;
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode temp=head;
+       while(fast.next!=null && fast.next.next!=null)
+       {
+           slow=slow.next;
+           fast=fast.next.next;
+           if(fast==slow)
+           {
+               slow=head;
+               while(slow!=fast)
+               {
+                   slow=slow.next;
+                   fast=fast.next;
+               }
+               return fast;
+           }
+       }
+        return null;
+        
+    }
+}
