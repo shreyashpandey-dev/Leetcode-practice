d9b38eeff2d04f8ef5388239a5b391273f1581d7
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-08-20 07:27:01 +0000

    [LeetCode Sync] Runtime - 2 ms (96.46%), Memory - 46.2 MB (100.00%)

--- my-folder/0338-counting-bits/solution.java
+++ my-folder/0338-counting-bits/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
-      int arr[]=new int[n+1];
-         arr[0]=0;
-       for(int i=1;i<=n;i++)
-       {
-           arr[i]=arr[i/2]+i%2;
-       }
+        int arr[]=new int[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            arr[i]=arr[i/2]+i%2;
+        }
         return arr;
     }
 }
