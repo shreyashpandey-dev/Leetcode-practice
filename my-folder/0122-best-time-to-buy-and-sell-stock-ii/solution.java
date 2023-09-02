1834bad662d242e91651826cab7cc32fda482f45
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-09-02 11:12:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (99.99%)

--- my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,33 +1,32 @@
 class Solution {
-   int profit=-2;
-    public int maxProfit(int[] prices) {
-       
+    public int maxProfit(int[] arr) {
+        int n=arr.length;
+       // int dp[][]=new int[n+1][2];
         int ahead[]=new int[2];
         int curr[]=new int[2];
-        ahead[0]=ahead[1]=0;
-       for(int ind=prices.length-1;ind>=0;ind--)
-       {
-           for(int buy=0;buy<=1;buy++)
-           {
-            if(buy==1)
-       {
-           profit=Math.max(-prices[ind]+ahead[0]//bought the stock on that day
-                           ,0+ahead[1]//not bought the stock on that day
-                          );
-    }
-    else
-    {
-        profit=Math.max(prices[ind]+ahead[1]//bought the stock on that day
-                           ,0+ahead[0]//not bought the stock on that day
-                          );
+        ahead[0]=ahead[1]=0; //base case
+        //dp[n][0]=dp[n][1]=0; //base case;
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                if(buy==1)
+                {
+                 int not_take=0+ahead[1];
+                 int take=-arr[ind]+ahead[0];
+                 curr[buy]=Math.max(take,not_take);  
+                }
+                else
+                {
+                 int not_take=0+ahead[0];
+                 int take=arr[ind]+ahead[1];
+                 curr[buy]=Math.max(take,not_take);  
+                }
+            }
+            ahead=curr;
+        }
         
-    }
-       curr[buy]=profit;
-       }
-          ahead=curr;
-       }
-           
+        return ahead[1];
         
-      return ahead[1];
     }
 }
