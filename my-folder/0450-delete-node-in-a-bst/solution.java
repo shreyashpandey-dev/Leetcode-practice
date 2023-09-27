2248b81640a4cde649d9ee11302c7c7157178fb0
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-09-27 10:37:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.1 MB (99.98%)

--- my-folder/0450-delete-node-in-a-bst/solution.java
+++ my-folder/0450-delete-node-in-a-bst/solution.java
@@ -14,33 +14,54 @@
  * }
  */
 class Solution {
-    public TreeNode getSucc(TreeNode root)
+    public TreeNode lastR(TreeNode root)
     {
-        TreeNode succ=root.right;
-        while(succ!=null && succ.left!=null)
-        succ=succ.left;
-        return succ;
+        if(root.right==null)
+        return root;
+        return lastR(root.right);
     }
-    public TreeNode deleteNode(TreeNode root, int x) {
-        if(root==null)
-        return null;
-        if(root.val>x)
-        root.left=deleteNode(root.left,x);
-         else if(root.val<x)
-        root.right=deleteNode(root.right,x);
-        else
-        {
+    public TreeNode helper(TreeNode root)
+    {
         if(root.left==null)
         return root.right;
-       else if(root.right==null)
+        if(root.right==null)
         return root.left;
-        else
+        TreeNode rightNode=root.right;
+        TreeNode lastRight=lastR(root.left);
+        lastRight.right=rightNode;
+        return root.left;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        TreeNode temp=root;
+        if(root==null)
+        return null;
+        if(root.val==key)
+        return helper(root);
+        while(root!=null)
         {
-            TreeNode succ=getSucc(root);
-            root.val=succ.val;
-            root.right=deleteNode(root.right,succ.val);
+            if(root.val>key)
+            {
+                if(root.left!=null && root.left.val==key)
+                {
+                root.left=helper(root.left);
+                break;
+                }
+                else
+                root=root.left;
+            }
+            else
+            {
+                if(root.right!=null && root.right.val==key)
+                {
+                    root.right=helper(root.right);
+                    break;
+                }
+                else
+                root=root.right;
+            }
         }
-        }
-        return root;
+        return temp;
+
+
     }
 }
