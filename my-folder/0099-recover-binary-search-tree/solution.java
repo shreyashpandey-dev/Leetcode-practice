1c4a67adc2ab103f7c8062831a5822eca58af8d2
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-09-30 18:40:19 +0000

    [LeetCode Sync] Runtime - 2 ms (17.73%), Memory - 43.2 MB (100.00%)

--- my-folder/0099-recover-binary-search-tree/solution.java
+++ my-folder/0099-recover-binary-search-tree/solution.java
@@ -14,43 +14,38 @@
  * }
  */
 class Solution {
-      int x=0,y=0;
-    ArrayList<Integer>arr=new ArrayList<>();
-    void inorder(TreeNode root)
+    TreeNode first=null;
+    TreeNode mid=null;
+    TreeNode last=null;
+    TreeNode prev=new TreeNode(Integer.MIN_VALUE);
+    public void swap(TreeNode a,TreeNode b)
+    {
+        int temp=a.val;
+        a.val=b.val;
+        b.val=temp;
+    }
+    public void inorder(TreeNode root)
     {
         if(root==null)
-        return ;
+        return;
         inorder(root.left);
-        arr.add(root.val);
+        if(prev.val>root.val && first==null)
+        {
+            first=prev;
+            mid=root;
+        }
+        else if(prev.val>root.val)
+        {
+            last=root;
+        }
+        prev=root;
         inorder(root.right);
     }
-    void inorder1(TreeNode root)
-    {
-        if(root==null)
-        return ;
-        inorder1(root.left);
-        if(root.val==x)
-        root.val=y;
-        else if(root.val==y)
-        root.val=x;
-        inorder1(root.right);
-    }
     public void recoverTree(TreeNode root) {
-      inorder(root);  
-     int ar[]=new int[arr.size()];
-     for(int i=0;i<arr.size();i++)
-     ar[i]=arr.get(i);
-     Arrays.sort(ar);
-   
-     for(int i=0;i<arr.size();i++)
-     {
-         if(arr.get(i)!=ar[i])
-         {
-          x=arr.get(i);
-          y=ar[i];
-          break;
-         }
-     }
-     inorder1(root);
+        inorder(root);
+        if(last==null)
+        swap(first,mid);
+        else
+        swap(first,last);
     }
 }
