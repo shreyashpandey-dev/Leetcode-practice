37818a6abdf69676d89e756d65c194b42e400a96
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-09-30 14:11:14 +0000

    [LeetCode Sync] Runtime - 2 ms (95.82%), Memory - 46.3 MB (99.95%)

---
+++ my-folder/0167-two-sum-ii---input-array-is-sorted/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int i=0;
+        int j=arr.length-1;
+        while(i<j)
+        {
+            // if(arr[i]==arr[j])
+            // j--;
+            if(arr[i]+arr[j]==target)
+            return new int[]{i+1,j+1};
+            else if(arr[i]+arr[j]>target)  
+            j--;
+            else
+            i++;
+        }
+        throw null;
+    }
+}
