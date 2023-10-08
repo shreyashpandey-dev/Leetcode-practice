e24ff16bbdf64246a740a7b20c6daf2127f4bf39
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-10-08 13:17:08 +0000

    [LeetCode Sync] Runtime - 58 ms (6.58%), Memory - 31.6 MB (100.00%)

---
+++ my-folder/1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int findNoOfDay(int capacity,vector<int>&arr)
+    {
+        int wtSum=0;
+        int Day=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(wtSum<capacity)
+            {
+                wtSum+=arr[i];
+            }
+           if(wtSum==capacity)
+            {
+                Day++;   
+                wtSum=0;
+            }
+            if(wtSum>capacity)
+            {
+                Day++;
+                wtSum=arr[i];
+            }
+        }
+        if(wtSum>0)
+        Day++;
+        return Day;
+    }
+    int shipWithinDays(vector<int>& arr, int d) {
+        int n=arr.size();
+        int low=0;
+        int high=0;
+        for(int i=0;i<n;i++)
+        {
+            if(low<arr[i])
+            low=arr[i];
+            high=high+arr[i];
+        }
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+           int day=findNoOfDay(mid,arr);
+           if(day<=d)
+           high=mid-1;
+           else
+           low=mid+1;
+        }
+        return low;
+        
+    }
+};
