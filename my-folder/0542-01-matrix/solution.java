6e44830f06afdf28798762c31edb9540c550bc72
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2023-11-07 13:29:05 +0000

    [LeetCode Sync] Runtime - 13 ms (91.44%), Memory - 45.3 MB (100.00%)

--- my-folder/0542-01-matrix/solution.java
+++ my-folder/0542-01-matrix/solution.java
@@ -13,14 +13,14 @@ class Solution {
         int n=mat.length;
         int m=mat[0].length;
         Queue<Pair>q=new LinkedList<>();
-        for(int i=0;i<mat.length;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<mat[i].length;j++)
+            for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
                 q.add(new Pair(i,j));
                 else
-                    mat[i][j]=-1; //unvisited
+                mat[i][j]=-1;
             }
         }
         while(!q.isEmpty())
@@ -28,19 +28,19 @@ class Solution {
             Pair p=q.poll();
             int x=p.i;
             int y=p.j;
-           int[] dx = {-1, 1, 0, 0};
-           int[] dy = {0, 0, -1, 1};
-            for (int i = 0; i < 4; i++) {
-                int newX = x + dx[i];
-                int newY = y + dy[i];
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m && mat[newX][newY] == -1)
-                {
-                    mat[newX][newY] = mat[x][y] + 1;
-                    q.add(new Pair(newX, newY));
-                }
+        int dx[]={1,-1,0,0};
+        int dy[]={0,0,-1,1};
+        for(int i=0;i<4;i++)
+        {
+            int newX=x+dx[i];
+            int newY=y+dy[i];
+            if(newX>=0 && newY>=0 && newX<n && newY<m && mat[newX][newY]==-1)
+            {
+                mat[newX][newY]=mat[x][y]+1;
+                q.add(new Pair(newX,newY));
             }
         }
+        }
         return mat;
-        
     }
 }
