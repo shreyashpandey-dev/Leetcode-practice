5471eddecf5c6dbe00cf32445c5af7fc2607ad4c
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2024-01-30 20:04:16 +0000

    [LeetCode Sync] Runtime - 3 ms (19.01%), Memory - 46.3 MB (99.92%)

--- my-folder/0283-move-zeroes/solution.java
+++ my-folder/0283-move-zeroes/solution.java
@@ -1,16 +1,31 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        int cnt_of_zero=0;
-        for(int i=0;i<arr.length;i++) 
-        {
-            if(arr[i]==0)
-                cnt_of_zero++;
-            else {
-                int temp=arr[i];
-                arr[i]=arr[i-cnt_of_zero];
-                arr[i-cnt_of_zero]=temp;
+        // int cnt_of_zero=0;
+        // for(int i=0;i<arr.length;i++) 
+        // {
+        //     if(arr[i]==0)
+        //         cnt_of_zero++;
+        //     else {
+        //         int temp=arr[i];
+        //         arr[i]=arr[i-cnt_of_zero];
+        //         arr[i-cnt_of_zero]=temp;
                 
+        //     }
+        // }
+        int opos=-1;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(opos==-1 &&arr[i]==0)
+            opos=i;
+            if(opos!=-1 && arr[i]!=0)
+            {
+                //swap(arr,opos,i);
+                int temp=arr[opos];
+                arr[opos]=arr[i];
+                arr[i]=temp;
+                opos=opos+1;
             }
         }
+       // return arr;
     }
 }
