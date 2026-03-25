e28fac6f268a5739ad49588e756346e1ae062b7c
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2026-03-25 19:07:01 +0000

    [LeetCode Sync] Runtime - 7 ms (99.52%), Memory - 46.5 MB (75.67%)

--- my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
@@ -8,30 +8,25 @@
  * }
  */
 class Solution {
-    TreeNode LCA;
-    public boolean findlca(TreeNode root,int a,int b)
+    TreeNode ans=null;
+    public boolean lca(TreeNode root, TreeNode l1, TreeNode l2)
     {
         if(root==null)
         return false;
-        boolean self=(root.val==a||root.val==b);
-        boolean left=findlca(root.left,a,b);
-        if(LCA!=null)
+        boolean left=lca(root.left,l1,l2);
+        if(ans!=null)
         return true;
-        boolean right=findlca(root.right,a,b);
-        if(LCA!=null)
+        boolean right=lca(root.right,l1,l2);
+        if(ans!=null)
         return true;
-        if(self&&left||self&&right||left&&right)
-        LCA=root;
-        return self||left||right;
-
+        boolean self=(root==l1||root==l2);
+        if((left&self) || (right&&self) ||(left &&right))
+        ans=root;
+        return left||right||self;
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int a=p.val;
-        int b=q.val;
-        findlca(root,a,b);
-        return LCA;
-
-
+        boolean res=lca(root,p,q);
+        return ans;
         
     }
 }
