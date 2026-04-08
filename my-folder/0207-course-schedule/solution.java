1e19f984c86c2f547896bb04e69e0e9234196fd4
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2026-04-08 16:16:09 +0000

    [LeetCode Sync] Runtime - 7 ms (57.20%), Memory - 47.2 MB (49.47%)

--- my-folder/0207-course-schedule/solution.java
+++ my-folder/0207-course-schedule/solution.java
@@ -1,37 +1,37 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] preReq) {
-        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
-        int n=preReq.length;
-        int indeg[]=new int[numCourses];
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int ind[]=new int[numCourses];
+        int n=prerequisites.length;
+        List<List<Integer>>adj =new ArrayList<>();
         for(int i=0;i<numCourses;i++)
-            adj.add(new ArrayList<>());
+        adj.add(new ArrayList<>());
         for(int i=0;i<n;i++)
         {
-           adj.get(preReq[i][1]).add(preReq[i][0]);
-            indeg[preReq[i][0]]++;
+        adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        ind[prerequisites[i][0]]++;
         }
-        Queue<Integer>q=new LinkedList<>();
+        Queue<Integer>q=new  LinkedList<>();
         for(int i=0;i<numCourses;i++)
         {
-            if(indeg[i]==0)
-                q.add(i);
+            if(ind[i]==0)
+            q.add(i);
         }
         int cnt=0;
         while(!q.isEmpty())
         {
-            int u=q.poll();
-            for(int v:adj.get(u))
-            {
-                if(indeg[v]>0)
-                    indeg[v]--;
-                if(indeg[v]==0)
-                    q.add(v);
-            }
-            cnt++;
+         cnt++;
+         int u=q.poll();
+         for(int v:adj.get(u))
+         {
+            ind[v]--;
+            if(ind[v]==0)
+            q.add(v);
+         }   
         }
         return cnt==numCourses;
         
-        
+
+
         
     }
 }
