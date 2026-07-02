6f1329e5214e9d615fd657cea7ae255c8cc65d18
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2026-07-02 15:36:34 +0000

    [LeetCode Sync] Runtime - 1 ms (94.05%), Memory - 43.6 MB (15.66%)

--- my-folder/0118-pascals-triangle/solution.java
+++ my-folder/0118-pascals-triangle/solution.java
@@ -1,20 +1,17 @@
 class Solution {
-    public List<List<Integer>> generate(int n) {
-        List<List<Integer>>arr=new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {
-            List<Integer>arr1=Arrays.asList(new Integer[i+1]);
-            for(int j=0;j<=i;j++)
-            {
-                if(j==0||j==i)
-                    arr1.set(j,1);
-                else
-                arr1.set(j,arr.get(i-1).get(j)+arr.get(i-1).get(j-1));
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
+                }
             }
-            arr.add(arr1);
-            
+            ans.add(row);
         }
-        return arr;
-        
+        return ans;
     }
 }
