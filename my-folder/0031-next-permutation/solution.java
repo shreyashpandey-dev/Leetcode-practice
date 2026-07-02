24a946d7e760eb77defc4535e08b9f089c650611
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2026-07-02 18:46:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.4 MB (83.90%)

--- my-folder/0031-next-permutation/solution.java
+++ my-folder/0031-next-permutation/solution.java
@@ -1,30 +1,33 @@
 class Solution {
-    public void swap(int arr[],int i,int j)
+    public void swap(int a,int b, int arr[])
     {
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-    public void rev(int[]arr,int a,int b)
-    {
-        while(a<b)
-            swap(arr,a++,b--);
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
     }
     public void nextPermutation(int[] arr) {
         int n=arr.length;
-        if(n==1)
-            return;
-        int i=arr.length-2;
-        while(i>=0 && arr[i]>=arr[i+1])
-            i--;
-        if(i>=0)
+        int i=n-1;
+        while(i!=0 && arr[i]<=arr[i-1])
+        i--;
+        int DecSeqInd=i;
+        if(i!=0)
         {
-        int j=n-1;
-        while(arr[j]<=arr[i])
-            j--;
-        swap(arr,i,j);
+        int pivot=i-1;
+        i=n-1;
+        while(arr[i]<=arr[pivot])
+        i--;
+        swap(i,pivot,arr);
         }
-        rev(arr,i+1,n-1);
+        int startRev=n-1;
+        int endRev=DecSeqInd;
+        while(startRev>endRev)
+        {
+            swap(startRev,endRev,arr);
+            startRev--;
+            endRev++;
+        }
+
         
     }
 }
