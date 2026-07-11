6c91ed446d9c023da083904fa85d6cc0bfb82e04
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2026-07-11 18:32:29 +0000

    [LeetCode Sync] Runtime - 2 ms (91.25%), Memory - 44.1 MB (57.05%)

--- my-folder/1036-rotting-oranges/solution.java
+++ my-folder/1036-rotting-oranges/solution.java
@@ -1,77 +1,75 @@
-class Pair
-{
-    int i;
-    int j;
-    Pair(int i,int j)
+class Pair{
+    int x,y;
+    Pair(int x, int y)
     {
-        this.i=i;
-        this.j=j;
+        this.x=x;
+        this.y=y;
     }
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<Pair>q=new LinkedList<>();
         int n=grid.length;
         int m=grid[0].length;
-        Queue<Pair>q=new LinkedList<>();
-        //ArrayList<Pair>arr=new ArrayList<>();
-        int total_oranges=0,rotten_oranges=0;
-        for(int i=0;i<grid.length;i++)
+        int totalC=0,rottenC=0;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<grid[i].length;j++)
+            for(int j=0;j<m;j++)
             {
+                if(grid[i][j]!=0)
+                {
+                totalC++;
                 if(grid[i][j]==2)
                 {
-                    q.add(new Pair(i,j));
-                    rotten_oranges++;
+                rottenC++;
+                q.add(new Pair(i,j));
                 }
-                if(grid[i][j]!=0)
-                    total_oranges++;
-                    
-                    
+                }
             }
         }
-        int cnt=0;
-        if(rotten_oranges==total_oranges)
-                return cnt;
+        if(totalC==rottenC)
+        return 0;
+        int min=0;
         while(!q.isEmpty())
         {
-            int size=q.size();
-            cnt++;
-            for(int i=0;i<size;i++)
+            int len=q.size();
+            for(int i=0;i<len;i++)
             {
-                
                 Pair p=q.poll();
-                if(p.i>0 && grid[p.i-1][p.j]==1)
+                int x=p.x;
+                int y=p.y;
+                if(x+1<n && grid[x+1][y]==1)
                 {
-                q.add(new Pair(p.i-1,p.j));
-                grid[p.i-1][p.j]=2;
-                    rotten_oranges++;
+                    grid[x+1][y]=2;
+                    q.add(new Pair(x+1,y));
+                    rottenC++;
                 }
-                if(p.i<n-1 && grid[p.i+1][p.j]==1)
+                if(y+1<m && grid[x][y+1]==1)
                 {
-                q.add(new Pair(p.i+1,p.j));   //neeche
-                grid[p.i+1][p.j]=2;
-                    rotten_oranges++;
+                    grid[x][y+1]=2;
+                    q.add(new Pair(x,y+1));
+                    rottenC++;
                 }
-                if(p.j>0 && grid[p.i][p.j-1]==1)
+                if(x-1>=0 && grid[x-1][y]==1)
                 {
-                q.add(new Pair(p.i,p.j-1)); //Baaye
-                grid[p.i][p.j-1]=2;
-                    rotten_oranges++;
+                    grid[x-1][y]=2;
+                    q.add(new Pair(x-1,y));
+                    rottenC++;
                 }
-                if(p.j<m-1 && grid[p.i][p.j+1]==1)
+                if(y-1>=0 && grid[x][y-1]==1)
                 {
-                q.add(new Pair(p.i,p.j+1)); //Daaye
-                grid[p.i][p.j+1]=2;
-                    rotten_oranges++;
-                }
-                
+                    grid[x][y-1]=2;
+                    q.add(new Pair(x,y-1));
+                    rottenC++;
+                } 
             }
-            if(rotten_oranges==total_oranges)
-                return cnt;
-             
+            min=min+1;
+            if(rottenC==totalC)
+            return min;
+
         }
-            return -1;
+        return -1;
+        
         
     }
 }
