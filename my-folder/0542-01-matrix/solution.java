01f42e95c752421e17f30e2e0e0b799f1353d1cf
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2026-07-13 14:30:46 +0000

    [LeetCode Sync] Runtime - 14 ms (70.59%), Memory - 49.3 MB (43.59%)

--- my-folder/0542-01-matrix/solution.java
+++ my-folder/0542-01-matrix/solution.java
@@ -1,24 +1,24 @@
-class Pair
-{
-    int i;
-    int j;
-    Pair(int i,int j)
+class Pair{
+    int x,y;
+    Pair(int x, int y)
     {
-        this.i=i;
-        this.j=j;
+        this.x=x;
+        this.y=y;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+        Queue<Pair>q=new LinkedList<>();
         int n=mat.length;
         int m=mat[0].length;
-        Queue<Pair>q=new LinkedList<>();
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
-                q.add(new Pair(i,j));
+                {
+                    q.add(new Pair(i,j));
+                }
                 else
                 mat[i][j]=-1;
             }
@@ -26,21 +26,23 @@ class Solution {
         while(!q.isEmpty())
         {
             Pair p=q.poll();
-            int x=p.i;
-            int y=p.j;
-        int dx[]={1,-1,0,0};
-        int dy[]={0,0,-1,1};
-        for(int i=0;i<4;i++)
-        {
-            int newX=x+dx[i];
-            int newY=y+dy[i];
-            if(newX>=0 && newY>=0 && newX<n && newY<m && mat[newX][newY]==-1)
+            int x=p.x;
+            int y=p.y;
+            int dx[]={1,-1,0,0};
+            int dy[]={0,0,-1,1};
+            for(int i=0;i<4;i++)
             {
-                mat[newX][newY]=mat[x][y]+1;
-                q.add(new Pair(newX,newY));
+                int newX=x+dx[i];
+                int newY=y+dy[i];
+                if(newX>=0 && newX<n && newY>=0 && newY<m && mat[newX][newY]==-1)
+                {
+                    mat[newX][newY]=mat[x][y]+1;
+                    q.add(new Pair(newX,newY));
+                }
             }
         }
-        }
         return mat;
+
+        
     }
 }
