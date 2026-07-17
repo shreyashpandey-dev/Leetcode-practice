2b5db07bf9ed25e65231f1f7922f9f92b67feaca
Shreyash Pandey <81769088+shreyashpandey-dev@users.noreply.github.com>
2026-07-17 19:35:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.7 MB (27.14%)

--- my-folder/0063-unique-paths-ii/solution.java
+++ my-folder/0063-unique-paths-ii/solution.java
@@ -1,34 +1,29 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] arr) {
-        int n=arr.length;
-        int m=arr[0].length;
-         int dp[][]=new int[n][m];
-        if(arr[0][0]==1)
-            return 0;
-        dp[0][0]=1;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(i==0 && j==0)
-                    continue;
-                if(arr[i][j]==1)
-                    dp[i][j]=0;
-                else
-                {
-                    int up=0;
-                    if(i>=1)
-                     up=dp[i-1][j];
-                    int left=0;
-                    if(j>=1)
-                     left=dp[i][j-1];
-                    dp[i][j]=up+left;
-                }
-                
-            }
-        }
-        return dp[n-1][m-1];
-        
+    int Path(int i,int j,int mat[][],int dp[][])
+    {
+        if(i<0 || j<0)
+        return 0;
+        if(i==0 && j==0)
+        return 1;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int left=0;
+        if(j-1>=0 && mat[i][j-1]==0)
+        left=Path(i,j-1,mat,dp);
+        int up=0;
+        if(i-1>=0 && mat[i-1][j]==0)
+        up=Path(i-1,j,mat,dp);
+        return dp[i][j]=up+left;
+    }
+    public int uniquePathsWithObstacles(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int dp[][]=new int[n][m];
+        if(mat[0][0]==1 || mat[n-1][m-1]==1)
+        return 0;
+        for(int[] rows: dp)
+        Arrays.fill(rows,-1);
+        return Path(n-1,m-1,mat,dp);
         
     }
 }
